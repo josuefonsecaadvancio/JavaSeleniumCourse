e0eedfce14b13ca20df9c14102594b1a406c0a86
josuefonsecaadvancio <123123623+josuefonsecaadvancio@users.noreply.github.com>
2023-01-23 16:07:42 -0800

    Homework123

--- src/test/java/firstCodeClass/test1.java
+++ src/test/java/firstCodeClass/test1.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class test1 {
     public static void main(String[] args){
         System.out.println("hello world");
@@ -22,6 +24,36 @@ public class test1 {
         passwordInput.sendKeys("secret_sauce");
         loginButton.click();
 
+        List<WebElement> allitems = driver.findElements(By.className("inventory_item_name"));
+
+        for (WebElement currentitem: allitems ) {
+
+            String curreitem_text = currentitem.getText();
+            System.out.println(curreitem_text);
+        }
+
+        /*WebElement backpack_addtocart = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]"));
+        WebElement bikelight_addtocart = driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-bike-light\"]"));
+        WebElement shoppingcart = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a"));
+
+        backpack_addtocart.click();
+        bikelight_addtocart.click();
+        shoppingcart.click();
+
+        boolean item1 = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).isDisplayed();
+        if (item1) {
+            System.out.println("Element backpack is displayed");
+        } else {
+            System.out.println("Element backpack is not displayed");
+        }
+
+        boolean item2 = driver.findElement(By.xpath("//*[@id=\"item_0_title_link\"]/div")).isDisplayed();
+        if (item2) {
+            System.out.println("Element bikelight is displayed");
+        } else {
+            System.out.println("Element bikelight is not displayed");
+        }*/
+
         //Use the code below, to match the expected text of the first item in the shop
         //Assert.assertEquals();
 
@@ -33,7 +65,7 @@ public class test1 {
             throw new RuntimeException(e);
         }
 
-        driver.quit();
+        //driver.quit();
 
 
     }
