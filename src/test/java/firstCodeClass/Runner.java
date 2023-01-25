3986d10db5839cc1a175a4fccbb8019df3fb3dae
Diego-Padilla-Advancio <101145072+Diego-Padilla-Advancio@users.noreply.github.com>
2023-01-24 22:40:40 -0800

    Added Runner class

---
+++ src/test/java/firstCodeClass/Runner.java
@@ -0,0 +1,10 @@
+package firstCodeClass;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Tests.class})
+public class Runner {
+
+}
